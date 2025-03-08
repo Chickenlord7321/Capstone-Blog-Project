4d30c7d99dcda4e5d642c43422b0f54c489790d4
Chickenlord7321 <118081050+Chickenlord7321@users.noreply.github.com>
2025-03-08 15:57:37 -0800

    Updated environment API keys

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ CURRENT_YEAR = datetime.now().strftime("%Y")
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')  # '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
